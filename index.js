66cec8515328efee32f790fee42ba501ab34f7cc
steven <szapata427@yahoo.com>
2018-11-03 13:48:33 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function Scooter(year, color, model) {
+
+this.year = year
+this.color = color
+this.model = model
+
+}
+
+let samson = new Scooter(2015, "red", "swoosh")
+
+
+function Driver (name, age, experience) {
+
+this.name = name
+this.age = age
+this.experience = experience
+
+
+}
+
+
+function PickupLocation(address, city) {
+
+this.address = address
+this.city = city
+
+}
